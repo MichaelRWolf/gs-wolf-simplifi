3cc1a376382bf82f7813429eadda535053a71dde
Michael R. Wolf <MichaelRWolf@att.net>
2024-09-21 16:54:38 -0400

    @@ npx jest - runs, but doesn't find class file

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,17 @@ module.exports = {
   //     tsconfig: 'tsconfig.json', // Ensures ES5 targeting
   //   },
   // Specify test file patterns and ensure both src and test files are compiled
-  testMatch: ['**/test/**/*.test.ts'],
-  setupFilesAfterEnv: ['./src/Transaction.ts'],  // Load Transaction class globally before tests run
+  testMatch: ['**/**/*.test.ts'],
+  // setupFiles: ['./Transaction.ts'],  // Load Transaction class globally before tests run
+  // setupFilesAfterEnv: ['./Transaction.ts'],  // Load Transaction class globally before tests run
+  // setupFilesAfterEnv: ['./src/Transaction.ts'],  // Load Transaction class globally before tests run
+  //setupFiles: ['./setupTests.ts'],  // Load global Transaction class
+  // setupFiles: ['./src/Transaction.ts', './setupTests.ts'],  // Load Transaction class globally before tests run
+  // setupFilesAfterEnv: ['./src/Transaction.ts', './setupTests.ts'],  // Load Transaction class globally before tests run
+  // setupFiles: ['./setupTests.ts'],  // Load global Transaction class
 
+  // setupFiles: [
+  //   './src/setupTests.ts',
+  //   './src/Transaction.ts'
+  // ],  // Load Transaction class globally before tests run
 };
--- test/Transaction.test.ts
+++
@@ -1,7 +0,0 @@
-describe('Transaction', () => {
-    it('should perform some functionality', () => {
-        // const instance = new Transaction();
-        expect(1 + 1 == 2).toBe(true);
-        // expect(instance.isSplit()).toBeTruthy();
-    });
-});
